7bd51dc013e29937dba61a03e17ffee16a8bbaa6
ibn alam <ibnalam6@gmail.com>
2022-01-30 14:49:38 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+let $btns=$('.project-area.button-group button');
+$btns.click(function(e){
+
+    $('.project-area.button-group button').removeClass('active');
+    e.target.classList.add('active');
+
+    let selector=$(e.target).attr('data-filter');
+    $('.project-area.grid').isotope({
+        filter:selector
+    });
+    
+    return false;
+
+});
+
+
+
+});
